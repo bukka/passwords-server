b2163f95dc8f5a09c557356ec7c33d80319650d5
Jakub Zelenka <bukka@php.net>
2019-01-27 19:10:41 +0000

    Fix integration test and babel setup

--- test/routes/password.test.js
+++ test/routes/password.test.js
@@ -2,20 +2,21 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import request from 'supertest';
 import express from 'express';
+import bodyParser from 'body-parser';
 import PasswordProvider from '../../providers/PasswordProvider';
 import router from '../../routes/password';
 
 const app = express();
-app.use('/passwords', router);
+app.use(bodyParser.json());
+app.use('/password', router);
 
-describe('Test password get route', () => {
+describe('Request /password', () => {
   it('should get the password by name', (done) => {
     sinon.stub(PasswordProvider.prototype, 'get').withArgs('test').resolves('abc');
 
     request(app)
-      .get('/passwords/test')
+      .get('/password/test')
       .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
       .expect(200, { password: 'abc' }, done);
   });
 
@@ -23,9 +24,9 @@ describe('Test password get route', () => {
     sinon.stub(PasswordProvider.prototype, 'save').resolves('ok');
 
     request(app)
-      .post('/passwords/test')
+      .post('/password/test')
       .send({password: 'abcd'})
-      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
       .expect(201, done);
   });
 });
